1c6ed42d701b7186eee6e0ef61eee8a6e47c774c
Natarajan Kanakasabapathy <Natarajan.Kanakasabapathy@rabobank.nl>
2023-02-20 14:06:43 +0100

    Removed unused imports

--- pythonProject/RegressionModels/EvaluateRegressionModels.py
+++ pythonProject/RegressionModels/EvaluateRegressionModels.py
@@ -1,9 +1,5 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-# Train the model
-from sklearn.linear_model import LinearRegression
 
 bike_data = pd.read_csv('Bike.csv')
 bike_data.head()
